279ab4a991c8d94f04dc3ded9e9215d16db15222
Vlad Stazhynski <stazhynski@gmail.com>
2020-08-07 13:57:46 +0100

    Made big refactoring

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def my_lower(s):
-    return s.lower()
-
-def correct_strings(arr):
-    output = []
-    for s in arr:
-        output.append(my_lower(s))
-    return np.array(output)
